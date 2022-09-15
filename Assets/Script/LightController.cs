289881587f56c086bd9dd496ef364d10439af842
carlacoronado207 <80068306+carlacoronado207@users.noreply.github.com>
2022-09-15 11:17:20 +0200

    Add lights prototype

---
+++ Assets/Script/LightController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public float time=10;
+    public GameObject lightGame;
+    float intensity;
+    float StartTime;
+    bool vivo=true;
+    
+    void Start()
+    {
+        StartTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vivo==true)
+        {
+            float TimerControl = Time.deltaTime - StartTime;
+
+            time -= TimerControl % 60;
+           // Debug.Log(time);
+            intensity = time /10f;
+            lightGame.GetComponent<Light>().intensity = intensity;
+            if (intensity <= 0)
+            {
+
+                vivo = false;
+                Debug.Log("Muere");
+            }
+        }
+      
+    }
+}
