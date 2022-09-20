f04f0498f2cb480bd8757129ec3dec4684b203fe
carlacoronado207 <80068306+carlacoronado207@users.noreply.github.com>
2022-09-20 11:34:58 +0200

    Update

---
+++ Assets/Script/CamaraController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraController : MonoBehaviour
+{
+	// Start is called before the first frame update
+	//Referencia a nuestro jugador
+	public GameObject jugador;
+
+	//Para registrar la diferencia entre la posición de la cámara y la del jugador
+	private Vector3 offset;
+
+	void Start()
+	{
+
+		//diferencia entre la posición de la cámara y la del jugador
+		offset = transform.position - jugador.transform.position;
+
+
+	}
+
+	// Se ejecuta cada frame, pero después de haber procesado todo. Es más exacto para la cámara
+	void LateUpdate()
+	{
+
+		//Actualizo la posición de la cámara
+		transform.position = jugador.transform.position + offset;
+
+	}
+}
