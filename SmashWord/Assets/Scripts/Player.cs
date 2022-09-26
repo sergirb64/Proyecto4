de6b2364901375248338e24be93d94d7da27e54c
Aimofe <aidamonfortef@gmail.com>
2022-09-27 00:51:36 +0200

    ataque enemigo y cambio de color

--- SmashWord/Assets/Scripts/Player.cs
+++ SmashWord/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    bool attack=true;
     public float life = 100;
    public  float enemyLife=100;
    public Image healthBar;
@@ -22,6 +23,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (attack)
+        {
+            StartCoroutine("AtaqueEnemigo");
+        }
+        playerText.Select();
         //if (Input.GetKeyDown(KeyCode.T))
         //   playerText.DeactivateInputField();
         if (Input.GetKeyDown(KeyCode.Return))
@@ -29,14 +35,24 @@ public class Player : MonoBehaviour
 
             {
                 textControler.GetComponent<TextControler>().highlightWords.Remove(playerText.text);
+                textControler.GetComponent<TextControler>().CompletedWords.Add(playerText.text);
+                textControler.GetComponent<TextControler>().dialogue.text = "";
+                textControler.GetComponent<TextControler>().RewriteText("<color=red>", "</color = red > ", playerText.text);
                 enemyLife -= 10;
                 playerText.text = "";
+                playerText.Select();
+            }
+            else if (playerText.text =="")
+            {
+               
             }
             else
             {
-                life -=10;
+                life -= 10;
                 playerText.text = "";
+                playerText.Select();
             }
+
         
         ;
         HealthBar(healthBar, maxLife, life);
@@ -47,5 +63,13 @@ public class Player : MonoBehaviour
     void HealthBar(Image bar,float max, float current)
     {
         bar.fillAmount = current / max;
+    }
+
+    IEnumerator AtaqueEnemigo()
+    {
+        attack = false;
+        yield return new WaitForSeconds(5f);
+        life -= 10;
+        attack = true;
     }
 }
--- SmashWord/Assets/Scripts/TextControler.cs
+++ SmashWord/Assets/Scripts/TextControler.cs
@@ -13,6 +13,7 @@ public class TextControler : MonoBehaviour
     public TextMeshProUGUI dialogue;
     public string[] source;
     public List<string> highlightWords;
+    public List<string> CompletedWords;
     int count=0;
     bool writing;
     //public class Counter
@@ -31,28 +32,29 @@ public class TextControler : MonoBehaviour
     void Start()
     {
         //dialogue.GetComponent<TextMeshPro>
-        Debug.Log(WordsFromText(dialogues[0]));
+        //Debug.Log(WordsFromText(dialogues[0]));
+        WordsFromText(dialogues[0]);
         dialogue.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (count <= source.Length-1)
+        if (count <= source.Length - 1)
         {
             writing = true;
             if (count % 5 == 0)
             {
-                Debug.Log(source[count] + count);
-               // int random = Random.Range(count, 4f);
-               
+                //Debug.Log(source[count] + count);
+                // int random = Random.Range(count, 4f);
+
                 dialogue.text += "<color=yellow>" + source[count] + "</color = yellow > ";
                 highlightWords.Add(source[count]);
                 count++;
             }
             else
             {
-                dialogue.text +=   source[count]+ " ";
+                dialogue.text += source[count] + " ";
                 count++;
             }
         }
@@ -63,11 +65,53 @@ public class TextControler : MonoBehaviour
 
         if (highlightWords.Count==0&&writing==false)
         {
+            CompletedWords.Clear();
             Debug.Log("null");
             dialogue.text = "";
+           
            dialogueCount++;
+            count = 0;
             WordsFromText(dialogues[dialogueCount]);
         }
         
+    }
+    public void RewriteText(string color1,string color2,string word)
+    {
+        for (int count = 0; count < source.Length; count++)
+        {
+            if (highlightWords.Contains(source[count]))
+            {
+               // Debug.Log(source[count] + count);
+                // int random = Random.Range(count, 4f);
+
+                dialogue.text += "<color=yellow>" + source[count] + "</color = yellow > ";
+
+               
+            }
+            else if (source[count] == word||CompletedWords.Contains(source[count]))
+            {
+                //Debug.Log(source[count] + count);
+                // int random = Random.Range(count, 4f);
+                //CompletedWords.Add(source[count]);
+                dialogue.text += color1 + source[count] + color2;
+
+                
+            }
+          
+            else
+            {
+                dialogue.text += source[count] + " ";
+               
+            }
+        }
+        if (count <= source.Length - 1)
+        {
+            writing = true;
+            
+        }
+        else
+        {
+            writing = false;
+        }
     }
 }
