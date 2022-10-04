ad9dc8683935fe14eaee1b3ba691811228ec67a6
Aimofe <78077349+Aimofe@users.noreply.github.com>
2022-10-04 12:55:06 +0200

    MovPlayer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float sesibilidadRaton=120f;
+    public float velocity;
+    Vector3 moveInput;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouse = Input.GetAxis("Mouse X") * Time.deltaTime * sesibilidadRaton;
+        transform.Rotate(0, mouse, 0);
+        moveInput = new Vector3(mouse, 0, 1);
+        rb.velocity = Vector3.forward * velocity*moveInput.sqrMagnitude;
+       // transform.Translate(Vector3.forward * Time.deltaTime+transform.position);
+       
     }
 }
