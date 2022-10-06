6df50f4db05b8351dc9631fc892d530153e1559d
Aimofe <78077349+Aimofe@users.noreply.github.com>
2022-10-06 10:16:05 +0200

    actualizacion

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,27 +10,31 @@ public class Player : MonoBehaviour
     Vector3 direction;
     Rigidbody rb;
     public float time;
+    public float ObjectTime;
     float StartTime;
     public bool vivo = true;
+    public GameObject patines;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         StartTime = Time.time;
+        ObjectTime = time;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        
 
         //rb.velocity = Vector3.forward * velocity*direction.sqrMagnitude;
 
         // transform.Translate(Vector3.forward * Time.deltaTime+transform.position);
         if (vivo == true)
         {
+            
             float mouse = Input.GetAxis("Mouse X") * Time.deltaTime * sesibilidadRaton;
-            float mouseRot = Input.GetAxis("Mouse X") * Time.deltaTime * velocityRot;
+            float mouseRot = Input.GetAxis("Mouse X") * Time.deltaTime * velocity;
             transform.Rotate(0, mouse, 0);
             direction = new Vector3(mouseRot, 0, 1 * Time.deltaTime * velocity);
             transform.Translate(direction);
@@ -38,6 +42,7 @@ public class Player : MonoBehaviour
             float TimerControl = Time.deltaTime - StartTime;
 
             time -= TimerControl % 60;
+            patines.transform.localScale = new Vector3(patines.transform.localScale.x, time/ ObjectTime, patines.transform.localScale.z);
             // Debug.Log(time);
 
             if (time <= 0)
@@ -54,17 +59,23 @@ public class Player : MonoBehaviour
         if (collision.gameObject.CompareTag("Jabon"))
         {
             velocity = 10;
-            
+            time += 10f;
+            //patines.Mes
+            Destroy(collision.gameObject);
+
         }
         else if (collision.gameObject.CompareTag("Hielo"))
         {
-            velocity = 20;
+            velocity =15;
+            time += 10f;
+            Destroy(collision.gameObject);
         }
         else if (collision.gameObject.CompareTag("Platano"))
         {
-            velocity = 30;
+            velocity = 20;
+            time += 10f;
+            Destroy(collision.gameObject);
         }
-        time += 10f;
-        Destroy(collision.gameObject);
+       
     }
 }
