d5254b64ea7fe4b01b48593a637db72fb8c27a2d
ccpdrawer <carmencasan2001@gmail.com>
2022-10-15 19:04:41 +0200

    Noise color filter

--- Assets/Scripts/Planet Generator/ColourGenerator.cs
+++ Assets/Scripts/Planet Generator/ColourGenerator.cs
@@ -7,13 +7,13 @@ public class ColourGenerator
     ColourSettings settings;
     Texture2D texture;
     const int textureResolution = 50;
-
+ 
     public void UpdateSettings (ColourSettings settings)
     {
         this.settings = settings;
-        if (texture == null)
+        if (texture == null || texture.height != settings.biomeColourSettings.biomes.Length)
         {
-           texture = new Texture2D(textureResolution, 1);
+           texture = new Texture2D(textureResolution, settings.biomeColourSettings.biomes.Length);
         }
     }
 
@@ -22,12 +22,40 @@ public class ColourGenerator
         settings.planetMaterial.SetVector("_elevationMinMax", new Vector4(elevationMinMax.Min, elevationMinMax.Max));
     }
 
+    public float BiomePercentFromPoint (Vector3 pointOnUnitSphere)
+    {
+        float heightPercent = (pointOnUnitSphere.y + 1) / 2;
+        float biomeIndex = 0;
+        int numBiomes = settings.biomeColourSettings.biomes.Length;
+
+        for (int i = 0; i < numBiomes; i++)
+        {
+            if (settings.biomeColourSettings.biomes[i].startHeight < heightPercent)
+            {
+                biomeIndex = i;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return biomeIndex / Mathf.Max(1, numBiomes - 1);
+    }
+
     public void UpdateColours() //Aplicamos color al shader
     {
-        Color[] colours = new Color[textureResolution];
-        for(int i = 0; i < textureResolution; i++)
+        Color[] colours = new Color[texture.width * texture.height];
+        int colourIndex = 0;
+        foreach (var biome in settings.biomeColourSettings.biomes)
         {
-            colours[i] = settings.gradient.Evaluate(i / (textureResolution - 1f));
+            for (int i = 0; i < textureResolution; i++)
+            {
+                Color gradientCol = biome.gradient.Evaluate(i / (textureResolution - 1f));
+                Color tintCol = biome.tint;
+                colours[colourIndex] = gradientCol * (1 - biome.tintPercent) + tintCol * biome.tintPercent;
+                colourIndex++;
+            }
         }
         texture.SetPixels(colours);
         texture.Apply();
--- Assets/Scripts/Planet Generator/ColourSettings.cs
+++ Assets/Scripts/Planet Generator/ColourSettings.cs
@@ -5,6 +5,26 @@ using UnityEngine;
 [CreateAssetMenu()]
 public class ColourSettings : ScriptableObject
 {
-    public Gradient gradient;
     public Material planetMaterial;
+    public BiomeColourSettings biomeColourSettings;
+
+    [System.Serializable]
+    public class BiomeColourSettings
+    {
+        public Biome[] biomes;
+        public NoiseSettings noise;
+        public float noiseOffset;
+        public float noiseStrength;
+
+        [System.Serializable]
+        public class Biome
+        {
+            public Gradient gradient;
+            public Color tint;
+            [Range(0,1)]
+            public float startHeight;
+            [Range(0,1)]
+            public float tintPercent;
+        }
+    }
 }
