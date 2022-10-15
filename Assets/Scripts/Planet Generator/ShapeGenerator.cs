4032188a14c66896d03f2c95807ae5934f201ac0
ccpdrawer <carmencasan2001@gmail.com>
2022-10-15 02:20:24 +0200

    Using first noise layer as a mask

--- Assets/Scripts/Planet Generator/ShapeGenerator.cs
+++ Assets/Scripts/Planet Generator/ShapeGenerator.cs
@@ -20,10 +20,25 @@ public class ShapeGenerator
 
     public Vector3 CalculatePointOnPlanet(Vector3 pointOnUnitSphere)
     {
+        float firstLayerValue = 1;
         float elevation = 0;
-        for (int i = 0; i < noiseFilters.Length; i++)
+
+        if (noiseFilters.Length > 1)
         {
-            elevation += noiseFilters[i].Evaluate(pointOnUnitSphere);
+            firstLayerValue = noiseFilters[1].Evaluate(pointOnUnitSphere); //definimos la segunda capa como máscara
+            if (settings.noiseLayers[1].enabled)
+            {
+                elevation = firstLayerValue;
+            }
+        }
+
+        for (int i = 2; i < noiseFilters.Length; i++)
+        {
+            if (settings.noiseLayers[i].enabled)
+            {
+                float mask = (settings.noiseLayers[i].useFirstLayerAsMask) ? firstLayerValue : 2;
+                elevation += noiseFilters[i].Evaluate(pointOnUnitSphere) * mask;
+            }
         }
         return pointOnUnitSphere * settings.planetRadius * (1 + elevation);
     }
